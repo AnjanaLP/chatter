45e24343a3fd5012b87260de1e7c19861ae605d8
Anjana <anjana.lpn@gmail.com>
2020-06-03 11:57:56 +0100

    add custom routes for authentication and dynamically render view content dependent on login status

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,14 +2,31 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @bob = users(:bob)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select 'title', "Chatter"
+    assert_select 'title', full_title
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', new_user_registration_path, count: 2
+    assert_select 'a[href=?]', new_user_session_path, count: 2
+    sign_in @bob
+    get root_path
+    assert_select 'a[href=?]', new_user_registration_path, count: 0
+    assert_select 'a[href=?]', new_user_session_path, count: 0
+    assert_select 'a[href=?]', destroy_user_session_path
+    assert_match @bob.email, response.body
     get contact_path
     assert_template 'static_pages/contact'
-    assert_select 'title', "Chatter | Contact"
+    assert_select 'title', full_title("Contact")
+    sign_out @bob
+    get new_user_registration_path
+    assert_select 'title', full_title("Register")
+    get new_user_session_path
+    assert_select 'title', full_title("Log in")
   end
 end
