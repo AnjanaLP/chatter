3e91cad5c95cb73d1ecf36b966d07bafd1b05e41
Anjana <anjana.lpn@gmail.com>
2020-06-04 20:09:15 +0100

    remove jQuery-turbolinks gem and add a new workaround

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,7 +1,9 @@
-$(function () {
-  'use strict'
+$(document).on('turbolinks:load', function() {
+  $(function () {
+    'use strict'
 
-  $('[data-toggle="offcanvas"]').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open')
+    $('[data-toggle="offcanvas"]').on('click', function () {
+      $('.offcanvas-collapse').toggleClass('open')
+    })
   })
-})
+});
