8e017baa9376bd699a8f55d3645a94ee4d988a3f
Anjana <anjana.lpn@gmail.com>
2020-06-07 14:12:01 +0100

    render the user's posts on their profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
-50.times do |n|
+User.create!(name: "Test User",
+             email: "test@example.com",
+             password: "foobar",
+             password_confirmation: "foobar")
+
+35.times do |n|
   User.create!(name: Faker::Name.name,
                email: "user_#{n}@example.com",
                password: "password",
                password_confirmation: "password")
+end
+
+users_with_posts = User.order(:created_at).take(5)
+35.times do
+  content = Faker::Lorem.paragraph(sentence_count: 7)
+  users_with_posts.each { |user| user.posts.create!(content: content) }
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -3,13 +3,20 @@ require 'test_helper'
 class UsersProfileTest < ActionDispatch::IntegrationTest
 
   def setup
-    @bob = users(:bob)
+    @alice = users(:alice)
   end
 
-  test "profile page" do
-    get user_path(@bob)
+  test "profile page including paginated posts" do
+    get user_path(@alice)
     assert_template 'users/show'
-    assert_select "title", full_title(@bob.name)
-    assert_select "h2", text: @bob.name
+    assert_select "title", full_title(@alice.name)
+    assert_select "h1", text: @alice.name
+    assert_match @alice.posts.count.to_s, response.body
+    assert_select 'ul.pagination'
+    @alice.posts.paginate(page: 1).each do |post|
+      assert_select 'i.fa-user'
+      assert_select 'a[href=?]', user_path(post.user), text: post.user.name
+      assert_match post.content, response.body
+    end
   end
 end
